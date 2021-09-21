775ca152f01fdcb4b0fde745d9b65f6c5bb0da31
Cicero Gasparini Brunelli <brunelli.cg@gmail.com>
2021-09-21 17:20:01 -0300

    products list

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -1,12 +1,43 @@
 <template>
-  <p>Lista</p>
+  <section class="produtos-container">
+    <div v-for="produto in produtos" :key="produto.id">
+      <img
+        v-if="produtos.foto"
+        :src="produtos.foto[0].scr"
+        :alt="produtos.foto[0].titulo"
+      />
+      <p class="preco">{{ produto.preco }}</p>
+      <h2 class="titulo">{{ produto.nome }}</h2>
+      <p class="preco">{{ produto.descricao }}</p>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
   name: "ProdutosLista",
+  data() {
+    return {
+      produtos: null,
+    };
+  },
+
+  methods: {
+    getProdutos() {
+      fetch("http://localhost:3000/produto")
+        .then((response) => response.json())
+        .then((response) => {
+          this.produtos = response;
+        });
+    },
+  },
+
+  created() {
+    this.getProdutos();
+  },
 };
 </script>
+
 
 <style>
 </style>
