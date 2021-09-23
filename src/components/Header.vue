c23c4931a3aa576dd8259e1f664feaaa78ce4e69
Cicero Gasparini Brunelli <brunelli.cg@gmail.com>
2021-09-22 23:32:31 -0300

    Login vuex and usuer

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,25 @@
 <template>
-  <nav>
-    <router-link to="/" class="logo">
-      <img src="@/assets/ranek.svg" alt="Ranek" />
-    </router-link>
-    <router-link to="/login" class="btn"> Vender / Login </router-link>
-  </nav>
+  <header>
+    <nav>
+      <router-link to="/" class="logo">
+        <img src="@/assets/ranek.svg" alt="Ranek" />
+      </router-link>
+      <router-link v-if="$store.state.login" class="btn" to="/usuario">{{
+        nome
+      }}</router-link>
+      <router-link v-else class="btn" to="/login">Vender / Login</router-link>
+    </nav>
+  </header>
 </template>
 
 <script>
 export default {
   name: "Header",
+  computed: {
+    nome() {
+      return this.$store.state.usuario.nome.replace(/ .*/, "");
+    },
+  },
 };
 </script>
 
